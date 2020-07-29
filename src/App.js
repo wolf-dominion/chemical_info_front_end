a81fd01bc7ccf6d7ed88317af66f3c8f3d5fe92b
wolf-dominion <hannahdruryutd@gmail.com>
2020-07-29 13:14:08 -0600

    Attemped to use Hook tutorial from Ben Awad on Youtube, doesn't seem to work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [isTrue, setIsTrue] = useState(true);
+  //const dataURL = "https://sheet.best/api/sheets/955ad1a5-948c-49c5-81f0-2f832fb1efbb"
+  //const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+    
+    useEffect(async () => {
+      const response = await fetch('https://api.randomuser.me/');
+      const info = await response.json();
+      const item = info.results[0];
+      setData(item);
+      console.log(info.results[0].name.first)
+      setLoading(false);
+    }, [data, loading]);
 
+  //const {data, loading} = useFetch();
 return (
     <div id="site-container">
       <h1>Chemical Information Site</h1>
-      <p>You clicked {count} times</p>
-      <p>isTrue is set to {isTrue == true ? "true" : "false"}</p>
-      <button onClick={() => setCount(count + 1)}>
+      {/* <button onClick={() => setData([...data, 'new element'])}>
         Click me
-      </button>
+      </button> */}
+      {loading ? <div>...Loading</div> : <div>{data.name.first}</div>}
     </div>
   );
 }
